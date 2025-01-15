7d3fa1bca8ff0538c311541c03521dacfb3b42ea
Kaleb Missmer <missmerk@moravian.edu>
2025-01-15 14:26:35 -0500

    added new test

--- grades.py
+++ grades.py
@@ -3,3 +3,8 @@ def compute_hw_average(grades):
     if len(grades) == 0:
         return 0
     return grades[0]
+
+def compute_hw_average(grades):
+  if len(grades) == 0:
+      return 0
+  return sum(grades) / len(grades)
